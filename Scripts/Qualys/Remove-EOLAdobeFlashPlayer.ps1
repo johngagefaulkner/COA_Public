1e9588c341c2d8dbb7834aa1f42cd69708589c2e
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2024-04-11 18:59:53 -0400

    Create Remove-EOLAdobeFlashPlayer.ps1

---
+++ Scripts/Qualys/Remove-EOLAdobeFlashPlayer.ps1
@@ -0,0 +1,113 @@
+$script:flag = 0
+
+# Function to check Adobe Flash Player
+
+function CheckFlashPlayer {
+
+    $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*", "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+
+    $apps = Get-ChildItem -Path $RegPath | Get-ItemProperty
+
+   
+
+    foreach ($app in $apps) {
+
+        $appName = $app.DisplayName
+
+        if ($appName -match "Flash") {
+
+            $script:flag++
+
+            UninstallFlashPlayer
+
+        }
+
+    }
+
+}
+
+ 
+
+# Function to uninstall Adobe Flash Player
+
+function UninstallFlashPlayer {
+
+    if ($script:flag -eq 1) {
+
+        Write-Host "$appName is installed"
+
+        Write-Host "Uninstalling Flash Player"
+
+       
+
+        try {
+
+            # Download the Flash Player Uninstaller
+
+            $uninstallerUrl = https://fpdownload.macromedia.com/get/flashplayer/current/support/uninstall_flash_player.exe
+
+            $uninstallerPath = "$env:TEMP\uninstall_flash_player.exe"
+
+            Invoke-WebRequest -Uri $uninstallerUrl -OutFile $uninstallerPath
+
+ 
+
+            # Run the Flash Player Uninstaller
+
+            Start-Process -FilePath $uninstallerPath -ArgumentList "/uninstall" -Wait
+
+           
+
+            # Clean up the downloaded uninstaller
+
+            Remove-Item -Path $uninstallerPath -Force
+
+           
+
+            # Re-check for Flash Player
+
+            CheckFlashPlayer
+
+        }
+
+        catch {
+
+            Write-Host "Failed to download or run the Flash Player Uninstaller."
+
+            $script:flag = 404
+
+            exit 404
+
+        }
+
+    }
+
+    elseif ($script:flag -gt 1) {
+
+        Write-Host "Failed to uninstall Flash Player."
+
+        exit 2
+
+    }
+
+}
+
+ 
+
+CheckFlashPlayer
+
+ 
+
+if ($script:flag -eq 0) {
+
+    Write-Host "Flash Player is not installed."
+
+}
+
+elseif ($script:flag -eq 1) {
+
+    Write-Host "Flash Player has been uninstalled."
+
+    exit 1
+
+}
