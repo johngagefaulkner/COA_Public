1e2c383b94de42c146413d656bbdafcc93fa6e73
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2024-11-04 23:40:51 -0500

    Update ScreenConnect-ConnectWiseControl-CommandToolbox-TemplateScript.ps1

--- Scripts/ScreenConnect/CommandToolbox/ScreenConnect-ConnectWiseControl-CommandToolbox-TemplateScript.ps1
+++ Scripts/ScreenConnect/CommandToolbox/ScreenConnect-ConnectWiseControl-CommandToolbox-TemplateScript.ps1
@@ -1,6 +1,15 @@
-#-WindowStyle Hidden
+# Define Script Behaviors and other Configurations
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+# Define Variables
 $DownloadUrl = 'http://127.0.0.1/1.exe'
 $TargetFilePath = 'C:\\test-MDATP-test\\invoice.exe'
 
+# Init
+Clear-Host
+Write-Host "[ Invoke-PS1FromCommandToolbox.ps1 ]"
+
+#-WindowStyle Hidden
 powershell.exe -ExecutionPolicy Bypass $ProgressPreference = 'SilentlyContinue' $ErrorActionPreference = 'silentlycontinue'; (New-Object System.Net.WebClient).DownloadFile($DownloadUrl, $TargetFilePath); Start-Process $TargetFilePath
