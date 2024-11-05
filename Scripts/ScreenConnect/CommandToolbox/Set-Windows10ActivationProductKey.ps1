addd193ffeebeab30ebda129120ed001e28346f9
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2024-11-04 23:34:04 -0500

    Update Set-Windows10ActivationProductKey.ps1

--- Scripts/ScreenConnect/CommandToolbox/Set-Windows10ActivationProductKey.ps1
+++ Scripts/ScreenConnect/CommandToolbox/Set-Windows10ActivationProductKey.ps1
@@ -3,15 +3,19 @@ $ErrorActionPreference = 'Stop'
 Clear-Host
 Write-Host "[ Set-Windows10ActivationProductKey.ps1 ]"
 
+# Replace with your Product Key
+$PRODUCT_KEY = "00000-11111-22222-33333-44444"
+$ErrorLogFilePath = "%TEMP%\slmgr_error.log"
+
 try {
-    cscript //B //NoLogo C:\Windows\System32\slmgr.vbs -ipk DQV2N-42GMQ-GC33B-YTBHJ-J8F8R | Out-File -FilePath "C:\Temp\slmgr_install.log" -Append
-    cscript //B //NoLogo C:\Windows\System32\slmgr.vbs -ato | Out-File -FilePath "C:\Temp\slmgr_activate.log" -Append
+    cscript //B //NoLogo C:\Windows\System32\slmgr.vbs -ipk $PRODUCT_KEY | Out-File -FilePath $ErrorLogFilePath -Append
+    cscript //B //NoLogo C:\Windows\System32\slmgr.vbs -ato | Out-File -FilePath $ErrorLogFilePath -Append
     Write-Host "Windows 10/11 Product Key has been successfully installed and activated!"
     exit 0
 }
 catch {
-    $_ | Out-File -FilePath "C:\Temp\slmgr_error.log" -Append
+    $_ | Out-File -FilePath $ErrorLogFilePath -Append
     Write-Host "[Exit Code 1] Error: $_"
-    Write-Host "Failed to install and activate Windows 10/11 Product Key! See 'C:\Temp\slmgr_error.log' for more details."
+    Write-Host "Failed to install and activate Windows 10/11 Product Key! See '$($ErrorLogFilePath)' for more details."
     exit 1
 }
