fb81d131c3152e1ce9375a1fda2cf9c1d477747e
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2024-11-04 23:36:40 -0500

    Update ScreenConnect-ConnectWiseControl-CommandToolbox-TemplateScript.ps1

--- Scripts/ScreenConnect/CommandToolbox/ScreenConnect-ConnectWiseControl-CommandToolbox-TemplateScript.ps1
+++ Scripts/ScreenConnect/CommandToolbox/ScreenConnect-ConnectWiseControl-CommandToolbox-TemplateScript.ps1
@@ -1,1 +1,6 @@
-powershell.exe -NoExit -ExecutionPolicy Bypass -WindowStyle Hidden $ErrorActionPreference = 'silentlycontinue'; (New-Object System.Net.WebClient).DownloadFile('http://127.0.0.1/1.exe', 'C:\\test-MDATP-test\\invoice.exe'); Start-Process 'C:\\test-MDATP-test\\invoice.exe'
+#-WindowStyle Hidden
+
+$DownloadUrl = 'http://127.0.0.1/1.exe'
+$TargetFilePath = 'C:\\test-MDATP-test\\invoice.exe'
+
+powershell.exe -ExecutionPolicy Bypass $ProgressPreference = 'SilentlyContinue' $ErrorActionPreference = 'silentlycontinue'; (New-Object System.Net.WebClient).DownloadFile($DownloadUrl, $TargetFilePath); Start-Process $TargetFilePath
